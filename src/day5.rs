46bd191934f32dbeb77cc6acbd061adaf8f64e2f
Krzysztof Bielikowicz <kbielikowicz98@gmail.com>
2025-12-06 23:38:46 +0000

    Functional cleanup

--- src/day5.rs
+++ src/day5.rs
@@ -12,49 +12,32 @@ fn parse_input(input: &str) -> (Vec<RangeInclusive<usize>>, Vec<usize>) {
         .map(|line| {
             let parts = line
                 .trim()
-                .split_once("-")
+                .split_once('-')
                 .expect("Invalid line, could not find - to split");
             let start: usize = parts.0.parse().expect("Invalid start");
             let end: usize = parts.1.parse().expect("Invalid end");
             // handle ranges that are backwards
-            if start > end {
-                return end..=start;
-            }
-            start..=end
+            start.min(end)..=start.max(end)
         })
         .collect::<Vec<_>>();
 
     // Ensure ranges are sorted by start
-    ranges.sort_by(|a, b| a.start().cmp(&b.start()));
+    ranges.sort_by_key(|range| *range.start());
 
-    let mut merged_ranges: Vec<RangeInclusive<usize>> = Vec::new();
-
-    // Merge overlapping ranges
-    let mut current_range: Option<RangeInclusive<usize>> = None;
-
-    for next_range in &ranges {
-        if let Some(cur_range) = &current_range {
-            // check if range overlaps with current range or is next to it
-            if *next_range.start() <= (cur_range.end() + 1) {
-                // make sure to take max as they could be subsets
-                current_range = Some(RangeInclusive::new(
-                    *cur_range.start(),
-                    *cur_range.end().max(next_range.end()),
-                ));
-            } else {
-                // otherwise push current range to merged_ranges
-                merged_ranges.push(cur_range.clone());
-                current_range = Some(next_range.clone());
+    let merged_ranges: Vec<RangeInclusive<usize>> =
+        ranges.into_iter().fold(Vec::new(), |mut acc, range| {
+            match acc.last_mut() {
+                // there is a previous range and it overlaps, merge
+                Some(last) if *range.start() <= (last.end() + 1) => {
+                    *last = RangeInclusive::new(*last.start(), *range.end().max(last.end()));
+                }
+                // initial case or range is not overlapping, just proceed to next
+                _ => {
+                    acc.push(range);
+                }
             }
-        } else {
-            current_range = Some(next_range.clone());
-        }
-    }
-
-    // include last range
-    if let Some(cur_range) = current_range {
-        merged_ranges.push(cur_range);
-    }
+            acc
+        });
 
     let values = values
         .lines()
@@ -98,7 +81,7 @@ fn count_all_fresh(ranges: &[RangeInclusive<usize>]) -> usize {
     ranges
         .iter()
         // inclusive so +1
-        .map(|range| (range.end() - range.start() + 1) as usize)
+        .map(|range| range.end() - range.start() + 1)
         .sum()
 }
 
