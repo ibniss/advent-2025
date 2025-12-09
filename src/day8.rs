d075c270eab2d0f5d8760ea5d724f117537034d4
Krzysztof Bielikowicz <kbielikowicz98@gmail.com>
2025-12-09 00:33:39 +0000

    D8P1

---
+++ src/day8.rs
@@ -0,0 +1,222 @@
+use crate::{grid::Grid, solution::Day};
+
+/// x,y,z coordinates
+#[derive(Hash, Clone, Copy, PartialEq, Eq, Debug)]
+struct Coord3D {
+    x: i64,
+    y: i64,
+    z: i64,
+}
+
+// implement debug
+impl std::fmt::Display for Coord3D {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{},{})", self.x, self.y, self.z)
+    }
+}
+
+impl Coord3D {
+    fn new(x: i64, y: i64, z: i64) -> Self {
+        Self { x, y, z }
+    }
+
+    fn distance(&self, other: &Self) -> f64 {
+        ((self.x - other.x).pow(2) as f64
+            + (self.y - other.y).pow(2) as f64
+            + (self.z - other.z).pow(2) as f64)
+            .sqrt()
+    }
+
+    fn from_str(s: &str) -> Self {
+        let mut parts = s.split(',');
+        Self::new(
+            parts.next().unwrap().parse().unwrap(),
+            parts.next().unwrap().parse().unwrap(),
+            parts.next().unwrap().parse().unwrap(),
+        )
+    }
+}
+
+fn parse_input(input: &str) -> Vec<Coord3D> {
+    input.lines().map(Coord3D::from_str).collect()
+}
+
+struct DisjointSet {
+    /// Parent IDX of element N
+    parent: Vec<usize>,
+    /// Size of set that element N belongs to. Only root numbers are meaningful.
+    size: Vec<usize>,
+}
+
+impl DisjointSet {
+    /// Preallocate N elements
+    fn new(size: usize) -> Self {
+        Self {
+            parent: vec![0; size],
+            size: vec![0; size],
+        }
+    }
+
+    /// Add a new element into a new set containing only the new element
+    fn make_set(&mut self, x: usize) {
+        self.parent[x] = x;
+        self.size[x] = 1;
+    }
+
+    /// Find the root element (representative) of element x
+    fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] != x {
+            // flatten
+            self.parent[x] = self.find(self.parent[x]);
+            return self.parent[x];
+        }
+
+        x
+    }
+
+    /// Replaces the set containing x and the set containing y with their union
+    fn union(&mut self, x: usize, y: usize) {
+        println!("Unioning {} and {}", x, y);
+
+        // find roots of both
+        let x = self.find(x);
+        let y = self.find(y);
+
+        println!("Root(x) = {}, Root(y) = {}", x, y);
+
+        // nothing to do, they already belong to the same set
+        if x == y {
+            return;
+        }
+
+        // swap so x has >= descendants than y
+        if self.size[x] < self.size[y] {
+            let (x, y) = (y, x);
+        }
+
+        // Make x the new root (y now points to x)
+        self.parent[y] = x;
+        self.size[x] = self.size[x] + self.size[y];
+    }
+
+    /// Find indexes of all roots, i.e. items where their parent is themselves
+    fn roots(&self) -> impl Iterator<Item = usize> {
+        self.parent
+            .iter()
+            .enumerate()
+            .filter_map(|(idx, &x)| if x == idx { Some(idx) } else { None })
+    }
+
+    fn get_size(&self, x: usize) -> usize {
+        self.size[x]
+    }
+
+    fn sets_count(&self) -> usize {
+        self.parent
+            .iter()
+            .enumerate()
+            .filter(|(idx, x)| *idx == **x)
+            .count()
+    }
+
+    fn circuit_sizes(&self) -> Vec<usize> {
+        let mut sizes: Vec<usize> = self
+            .parent
+            .iter()
+            .enumerate()
+            .filter(|(idx, x)| *idx == **x && self.size[*idx] > 1)
+            .map(|(idx, _)| self.size[idx])
+            .collect();
+        sizes.sort_by(|a, b| b.cmp(a));
+        sizes
+    }
+}
+
+fn solve(boxes: &[Coord3D], iterations: usize) -> u64 {
+    let mut set = DisjointSet::new(boxes.len());
+
+    // first add all boxes to individual circuits
+    (0..boxes.len()).for_each(|idx| set.make_set(idx));
+
+    // prepare list of pairs (N^2) of ((idx, idx, distance) between each point, sorted so it's
+    // shortest
+    let mut pairs = Vec::new();
+    for i in 0..boxes.len() {
+        for j in (i + 1)..boxes.len() {
+            // skip same element
+            if i == j {
+                continue;
+            }
+
+            let distance = boxes[i].distance(&boxes[j]);
+            pairs.push((i, j, distance));
+        }
+    }
+    pairs.sort_by(|a, b| a.2.total_cmp(&b.2));
+
+    // then for each iteration (or however many boxes we have)
+    let max_iters = pairs.len().min(iterations);
+    (0..max_iters).for_each(|iter_idx| {
+        let (x_idx, y_idx, _) = pairs[iter_idx];
+        println!("\n\n{} -> {}", &boxes[x_idx], &boxes[y_idx]);
+
+        // connect x and y to same circuit
+        set.union(x_idx, y_idx);
+
+        println!("Circuits: {:?}", set.circuit_sizes());
+    });
+
+    // after we're done, find 3 largest circuits;
+    // for each root, gets its size
+    set.circuit_sizes()
+        .iter()
+        .take(3)
+        .fold(1u64, |acc, x| acc * (*x as u64))
+}
+
+pub struct Solution;
+
+impl Day for Solution {
+    fn part1(input: &str) -> crate::solution::Solution {
+        let coords = parse_input(&input);
+        solve(&coords, 1000).into()
+    }
+
+    fn part2(input: &str) -> crate::solution::Solution {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "\
+162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689";
+
+    #[test]
+    fn test_solve() {
+        let coords = parse_input(TEST_INPUT);
+        let result = solve(&coords, 10);
+        assert_eq!(result, 40);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,7 @@ macro_rules! register_days {
 }
 
 // Register all implemented days - just list them in order!
-register_days!(day1, day2, day3, day4, day5, day6, day7);
+register_days!(day1, day2, day3, day4, day5, day6, day7, day8);
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
